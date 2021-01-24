fea7841ac3d60f0528cdaab4c21a70fa0731eda2
Kate <kate.yakate@dmail.com>
2021-01-24 23:53:20 +0200

    Cycle practice

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 "use strict";
 
-const num = 50; 
+let num = 50; 
 
-// if (num < 40) {
-// 	console.log("Error!");
-// } else if (num > 100) {
-// 	console.log("Too much");
-// } else {
-// 	console.log("Ok");
+// while (num <= 55) {
+// 	console.log(num);
+// 	num++;
+// } 
+
+// do {
+// 	console.log(num);
+// 	num++;
 // }
+// while (num < 55);
 
-// (num === 100) ? console.log("Ok") : console.log("Error");
-
-switch (num) {
-	case 49: 
-		console.log("Неверно");
-		break;
-	case 100:
-		console.log("Неверно");
-		break;
-	case 50:
-		console.log("Правда");
-		break;
-	default:
-		console.log("Ничего не подходит");
-		break;
+for (let i = 1; i < 10; i++) {
+	if (i === 6) {
+		// break;
+		continue;
+	}
+	console.log(i);
 }
